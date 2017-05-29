b0cff3127f606954badb6148ebac14cfa8494463
Ensath <nfreed4@gmail.com>
2017-05-29 16:41:29 -0700

    Produces images within about 1 mile of PSU

--- app/src/main/java/com/example/mapdemo/StreetViewPanoramaBasicDemoActivity.java
+++ app/src/main/java/com/example/mapdemo/StreetViewPanoramaBasicDemoActivity.java
@@ -23,6 +23,10 @@ import com.google.android.gms.maps.model.LatLng;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.random;
 
 /**
  * This shows how to create a simple activity with streetview
@@ -32,11 +36,29 @@ public class StreetViewPanoramaBasicDemoActivity extends AppCompatActivity {
     // George St, Sydney
     private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
     private static final LatLng PSU = new LatLng(45.5110, -122.6832);
+    private static final double EARTHRADIUS = 3959;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.street_view_panorama_basic_demo);
+        LatLng start = PSU;
+        double angle = 2 * PI * random();
+        double distance = .7525 + .2508 * random();
+        double startLatRad = start.latitude * PI/180;
+        double startLngRad = start.longitude * PI/180;
+
+        // Distance to LatLng calculation based on "Destination point given distance and bearing from start point" formula
+        // at http://www.movable-type.co.uk/scripts/latlong.html by Chris Veness
+        double destLatRad =  Math.asin( Math.sin(startLatRad)*Math.cos(distance/EARTHRADIUS) +
+                Math.cos(startLatRad)*Math.sin(distance/EARTHRADIUS)*Math.cos(angle) );
+        double destLngRad = startLngRad + Math.atan2(Math.sin(angle)*Math.sin(distance/EARTHRADIUS)*Math.cos(startLatRad),
+                Math.cos(distance/EARTHRADIUS)-Math.sin(startLatRad)*Math.sin(destLatRad));
+
+        double destLat = destLatRad * 180/PI;
+        double destLng = destLngRad * 180/PI;
+        final LatLng destination = new LatLng(destLat, destLng);
+        Log.d("Destination:", destination.toString());
 
         SupportStreetViewPanoramaFragment streetViewPanoramaFragment =
                 (SupportStreetViewPanoramaFragment)
@@ -48,7 +70,7 @@ public class StreetViewPanoramaBasicDemoActivity extends AppCompatActivity {
                         // Only set the panorama to SYDNEY on startup (when no panoramas have been
                         // loaded which is when the savedInstanceState is null).
                         if (savedInstanceState == null) {
-                            panorama.setPosition(PSU);
+                            panorama.setPosition(destination);
                         }
                     }
                 });
