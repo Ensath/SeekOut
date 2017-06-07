941cfe43a6d7b62557624fe54819b542683f076d
Ensath <nfreed4@gmail.com>
2017-06-06 17:59:43 -0700

    Got users latitude and longitude! :)

--- app/src/main/java/com/example/mapdemo/StreetViewPanoramaBasicDemoActivity.java
+++ app/src/main/java/com/example/mapdemo/StreetViewPanoramaBasicDemoActivity.java
@@ -16,12 +16,23 @@
 
 package com.example.mapdemo;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
 import com.google.android.gms.maps.StreetViewPanorama;
 import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -31,32 +42,50 @@ import static java.lang.Math.random;
 /**
  * This shows how to create a simple activity with streetview
  */
-public class StreetViewPanoramaBasicDemoActivity extends AppCompatActivity {
+public class StreetViewPanoramaBasicDemoActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     // George St, Sydney
     private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
     private static final LatLng PSU = new LatLng(45.5110, -122.6832);
     private static final double EARTHRADIUS = 3959;
+    GoogleApiClient mGoogleApiClient = null;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.street_view_panorama_basic_demo);
-        LatLng start = PSU;
+        enableMyLocation();
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            Log.d("onCreate", "Creating GoogleApiClient");
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        /*LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();*/
+        LatLng start = PSU; //new LatLng(latitude, longitude);
         double angle = 2 * PI * random();
         double distance = .7525 + .2508 * random();
-        double startLatRad = start.latitude * PI/180;
-        double startLngRad = start.longitude * PI/180;
+        double startLatRad = start.latitude * PI / 180;
+        double startLngRad = start.longitude * PI / 180;
 
         // Distance to LatLng calculation based on "Destination point given distance and bearing from start point" formula
         // at http://www.movable-type.co.uk/scripts/latlong.html by Chris Veness
-        double destLatRad =  Math.asin( Math.sin(startLatRad)*Math.cos(distance/EARTHRADIUS) +
-                Math.cos(startLatRad)*Math.sin(distance/EARTHRADIUS)*Math.cos(angle) );
-        double destLngRad = startLngRad + Math.atan2(Math.sin(angle)*Math.sin(distance/EARTHRADIUS)*Math.cos(startLatRad),
-                Math.cos(distance/EARTHRADIUS)-Math.sin(startLatRad)*Math.sin(destLatRad));
+        double destLatRad = Math.asin(Math.sin(startLatRad) * Math.cos(distance / EARTHRADIUS) +
+                Math.cos(startLatRad) * Math.sin(distance / EARTHRADIUS) * Math.cos(angle));
+        double destLngRad = startLngRad + Math.atan2(Math.sin(angle) * Math.sin(distance / EARTHRADIUS) * Math.cos(startLatRad),
+                Math.cos(distance / EARTHRADIUS) - Math.sin(startLatRad) * Math.sin(destLatRad));
 
-        double destLat = destLatRad * 180/PI;
-        double destLng = destLngRad * 180/PI;
+        double destLat = destLatRad * 180 / PI;
+        double destLng = destLngRad * 180 / PI;
         final LatLng destination = new LatLng(destLat, destLng);
         Log.d("Destination:", destination.toString());
 
@@ -74,5 +103,57 @@ public class StreetViewPanoramaBasicDemoActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        }
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d("onConnected", "Connected");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Log.d("onConnected", "Have permission");
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            Log.d("onConnected", "mLastLocation notnull");
+            Log.d("Lat",String.valueOf(mLastLocation.getLatitude()));
+            Log.d("Lon",String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
